bc46cfecd283155517ee0428f635c5dd6cdf03dd
Howepu <125370383+Howepu@users.noreply.github.com>
2024-11-23 17:07:50 +0300

    Добавление примитивов

---
+++ src/main/java/backend/academy/flame/entities/Pixel.java
@@ -0,0 +1,16 @@
+package backend.academy.flame.entities;
+
+public record Pixel(int r, int g, int b, int hitCount) {
+    private static final int D = 255;
+
+    public Pixel addColor(int r, int g, int b) {
+        return new Pixel(
+            Math.min(D, Math.max(0, this.r + r)),
+            Math.min(D, Math.max(0, this.g + g)),
+            Math.min(D, Math.max(0, this.b + b)),
+            this.hitCount + 1
+        );
+    }
+}
+
+
---
+++ src/main/java/backend/academy/flame/entities/Point.java
@@ -0,0 +1,3 @@
+package backend.academy.flame.entities;
+
+public record Point(double x, double y) {}
