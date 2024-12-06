feb42218f0748b95d3b0601fffde253d05b53395
Howepu <125370383+Howepu@users.noreply.github.com>
2024-12-06 18:38:24 +0600

    Изменение рендеринга фрактала

--- src/main/java/backend/academy/flame/FractalApp.java
+++ src/main/java/backend/academy/flame/FractalApp.java
@@ -3,17 +3,14 @@ package backend.academy.flame;
 import backend.academy.flame.image.FractalRenderer;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Random;
 import java.util.Scanner;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @SuppressWarnings({"checkstyle:UncommentedMain", "checkstyle:MagicNumber"})
 public class FractalApp {
-    private static final int SAMPLES = 400000;
     private static final int THREADS = 4;
-    private static final String CORRECTION = "Применяем гамма-коррекцию к изображению...";
-    private static final Random RANDOM = new Random();
+
 
     private FractalApp() {
         throw new AssertionError("Не удается создать экземпляр служебного класса");
@@ -28,81 +25,17 @@ public class FractalApp {
         Path outputPath = Paths.get("fractal_flame");
 
         // Проверка ширины
-        int width = Checker.readPositiveInt(scanner, "Введите ширину изображения (например, 1920):");
+        int width = Checker.readPositiveInt(scanner, "Введите ширину изображения (рекомендовано 1920):");
 
         // Проверка высоты
-        int height = Checker.readPositiveInt(scanner, "Введите высоту изображения (например, 1080):");
+        int height = Checker.readPositiveInt(scanner, "Введите высоту изображения (рекомендовано 1080):");
 
         // Проверка количества итераций
-        int iterations = Checker.readPositiveInt(scanner, "Введите количество итераций (например, 50):");
+        int iterations = Checker.readPositiveInt(scanner, "Введите количество итераций (рекомендовано 5_000_000):");
 
-        // Проверка формата изображения
-        String format = Checker.readImageFormat(scanner);
-        outputPath = Path.of(outputPath + "." + format.toLowerCase());
+        FractalRenderer.createFlamePic(width, height, iterations);
 
-        // Ввод трансформаций
-
-        log.info("Введите трансформацию: (1-Eyefish, 2-Heart, 3-Waves, 4-Tangent, 5-Cross, 6-Linear, 7-Circular, "
-            + "8-Swirl, 9-Spiral)");
-
-//        int tr = Checker.readPositiveInt(scanner, "Введите номер трансформации:");
-//
-//        // Переменная для одной трансформации
-//       Transformation transformation = null;
-//
-//        switch (tr) {
-//            case 1 -> transformation = new EyefishTransformation();
-//            case 2 -> transformation = new HeartTransformation();
-//            //case 3 -> transformation = new SpiralTransformation();
-//            //case 4 -> transformation = new SwirlTransformation();
-//            case 5 -> transformation = new WavesTransformation();
-//            case 6 -> transformation = new TangentTransformation();
-//            case 7 -> transformation = new CrossTransformation();
-//            case 8 -> transformation = new HyperbolicTransformation();
-//            case 9 -> transformation = new CircularTransformation();
-//            default -> log.warn("Неизвестная трансформация: {}", tr);
-//        }
-
-        FractalRenderer.renderSingleThreaded(SAMPLES, iterations, width, height, RANDOM);
-
-//        log.info("Выберите режим запуска: (1-многопоточный или 2-однопоточный)");
-//        int threading = Checker.readOneOrTwo(scanner);
-//        if (threading == 1) {
-//            // Многопоточная обработка
-//            log.info("Начинаем многопоточную обработку...");
-//            long startTimeMulti = System.currentTimeMillis();
-//            FractalImage renderedImageMultiThreaded = renderer.renderMultithreaded(canvas, world, transformations,
-//                SAMPLES, iterations, THREADS);
-//            long endTimeMulti = System.currentTimeMillis();
-//            log.info("Многопоточная обработка заняла: {} мс", (endTimeMulti - startTimeMulti));
-//            // Пост-обработка (гамма-коррекция)
-//            log.info(CORRECTION);
-//            ImageProcessor gammaProcessor = new GammaCorrection(5, 5);
-//            gammaProcessor.process(renderedImageMultiThreaded);
-//            // Сохранение изображения
-//            ImageUtils.save(renderedImageMultiThreaded, outputPath, ImageFormat.valueOf(format));
-//        } else {
-//            // Однопоточная обработка
-//            log.info("Начинаем однопоточную обработку...");
-//            long startTime = System.currentTimeMillis();
-//            FractalImage renderedImageSingleThreaded = renderer.renderSingleThreaded(canvas, world, transformations,
-//                SAMPLES, iterations);
-//            long endTime = System.currentTimeMillis();
-//            log.info("Однопоточная обработка заняла: {} мс", (endTime - startTime));
-//            // Пост-обработка (гамма-коррекция)
-//            log.info(CORRECTION);
-//            ImageProcessor gammaProcessor = new GammaCorrection(5, 5);
-//            gammaProcessor.process(renderedImageSingleThreaded);
-//            // Сохранение изображения
-//            ImageUtils.save(renderedImageSingleThreaded, outputPath, ImageFormat.valueOf(format));
-//        }
-//        log.info("Фрактал сохранен в {}", outputPath);
-//
-//        // Подтверждение завершения программы
-//        log.info("Программа завершена. Выход.");
-//    }
-
-    }
+   }
 
     private static void logSystemConfiguration() {
         Runtime runtime = Runtime.getRuntime();
--- src/main/java/backend/academy/flame/image/FractalRenderer.java
+++ src/main/java/backend/academy/flame/image/FractalRenderer.java
@@ -5,6 +5,8 @@ import backend.academy.flame.entities.Pixel;
 import backend.academy.flame.entities.Point;
 import backend.academy.flame.transformations.CircularTransformation;
 import backend.academy.flame.transformations.CrossTransformation;
+import backend.academy.flame.transformations.EyeFishTransformation;
+import backend.academy.flame.transformations.HeartTransformation;
 import backend.academy.flame.transformations.LinearTransformation;
 import backend.academy.flame.transformations.SpiralTransformation;
 import backend.academy.flame.transformations.SwirlTransformation;
@@ -14,125 +16,162 @@ import backend.academy.flame.transformations.WavesTransformation;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import javax.imageio.ImageIO;
 import lombok.extern.slf4j.Slf4j;
 import static java.lang.Math.log10;
+import static java.lang.Math.pow;
 
+@SuppressWarnings({"checkstyle:MagicNumber", "checkstyle:MultipleStringLiterals", "checkstyle:NestedIfDepth"})
 @Slf4j
-@SuppressWarnings({"checkstyle:MagicNumber", "checkstyle:ParameterAssignment", "checkstyle:ReturnCount",
-    "checkstyle:NestedIfDepth"})
 public class FractalRenderer {
+    private static final double X_MIN = -1.777;
+    private static final double X_MAX = 1.777;
+    private static final double Y_MIN = -1;
+    private static final double Y_MAX = 1;
+    private static final int SAMPLES = 30;
+    private static final Random RANDOM = new Random();
+    private static Transformation[] linearTransformations;
+    private static Pixel[][] pixels;
+    private static BufferedImage image;
+    private static Scanner scanner = new Scanner(System.in);
 
     private FractalRenderer() {
         throw new AssertionError("Не удается создать экземпляр служебного класса");
     }
 
-    private static final double X_MIN = -1.777;
-    private static final double X_MAX = 1.777;
-    private static final double Y_MIN = -1;
-    private static final double Y_MAX = 1;
-    private static Pixel[][] pixels;
-    private static BufferedImage image;
-    private static Transformation[] linearTransformations;
-    private static Scanner scanner = new Scanner(System.in);
+    public static void createFlamePic(int width, int height, int it) {
+        long startTime = System.nanoTime(); // Замер времени начала
 
+        linearTransformations = new Transformation[SAMPLES];
+        Arrays.setAll(linearTransformations, i -> new LinearTransformation());
 
-    // Однопоточная версия рендеринга
-    public static void renderSingleThreaded(int samples, int iterations, int xRes, int yRes, Random random) {
-        Transformation transformation = null;
-
-        int tr = Checker.readPositiveInt(scanner, "Введите номер трансформации:");
-        linearTransformations = new Transformation[30];
-        Arrays.setAll(linearTransformations, i -> getRandomTransformation(tr));
-        pixels = new Pixel[xRes][yRes]; // Используем заданные размеры
-        for (int i = 0; i < xRes; i++) {
+        pixels = new Pixel[width][height];
+        for (int i = 0; i < width; i++) {
             Arrays.setAll(pixels[i], j -> new Pixel());
         }
-        image = new BufferedImage(xRes, yRes, BufferedImage.TYPE_INT_ARGB);
-        image.setRGB(0, 0, xRes, yRes, processPoint(samples, iterations, xRes, yRes, random), 0, xRes);
 
-        // Применение гамма-коррекции
-        applyGammaCorrection(xRes, yRes, 0.8);
+        image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        log.info("Выберите режим обработки (1 - многопоточный, 2 - однопоточный):");
+        int choice = Checker.readOneOrTwo(scanner);
 
+        int[] rgbData;
+        if (choice == 1) {
+            rgbData = processPointsMultithreaded(SAMPLES, it, width, height);
+        } else {
+            rgbData = processPointsSingleThreaded(SAMPLES, it, width, height);
+        }
 
-        File outputFile = new File("output_image.png");
+        image.setRGB(0, 0, width, height, rgbData, 0, width);
+
+        File resourcesDir = new File("src/main/resources/pics");
+        if (!resourcesDir.exists()) {
+            log.info("Папка resources не существует. Создаём её.");
+            resourcesDir.mkdirs();
+        }
+
+        File outputFile = new File(resourcesDir, "output_image.png");
         try {
             ImageIO.write(image, "png", outputFile);
-            log.info("Изображение успешно сохранено: {}", outputFile.getAbsolutePath());
+            log.info("Изображение успешно сохранено: {} ", outputFile.getAbsolutePath());
         } catch (IOException e) {
-            log.error("Ошибка сохранения изображения: {}", e.getMessage());
+            log.info("Ошибка сохранения изображения: {} ", e.getMessage());
         }
+
+        long endTime = System.nanoTime(); // Замер времени окончания
+        double duration = (endTime - startTime) / 1_000_000_000.0; // Перевод в секунды
+        log.info("Время выполнения: {} секунд", duration);
     }
 
-    // Метод для гамма-коррекции
-    private static void applyGammaCorrection(int xRes, int yRes, double gamma) {
-        double max = 0.0;
+    public static int[] processPointsMultithreaded(int n, int it, int xRes, int yRes) {
+        int tr = Checker.readPositiveInt(scanner, "Введите номер трансформации:");
 
-        // Находим максимальное значение нормализованного яркости
-        for (int row = 0; row < xRes; row++) {
-            for (int col = 0; col < yRes; col++) {
-                Pixel pixel = pixels[row][col];
-                if (pixel.counter() != 0) {
-                    pixel.normal(log10(pixel.counter())); // Применяем нормализацию
-                    if (pixel.normal() > max) {
-                        max = pixel.normal();
-                    }
+        int threadsCount = Runtime.getRuntime().availableProcessors();
+        ExecutorService executor = Executors.newFixedThreadPool(threadsCount);
+
+        Pixel[][][] threadPixels = new Pixel[threadsCount][xRes][yRes];
+        for (int i = 0; i < threadsCount; i++) {
+            for (int x = 0; x < xRes; x++) {
+                for (int y = 0; y < yRes; y++) {
+                    threadPixels[i][x][y] = new Pixel();
                 }
             }
         }
 
-        // Применяем гамма-коррекцию ко всем пикселям
-        for (int row = 0; row < xRes; row++) {
-            for (int col = 0; col < yRes; col++) {
-                Pixel pixel = pixels[row][col];
-                pixel.normal(pixel.normal() / max); // Нормализуем яркость
+        int blockSize = n / threadsCount;
+        List<Future<Void>> futures = new ArrayList<>();
 
-                // Применяем гамма-коррекцию к цветам
-                pixel.red(clamp((int) (pixel.red() * Math.pow(pixel.normal(), 1.0 / gamma))));
-                pixel.green(clamp((int) (pixel.green() * Math.pow(pixel.normal(), 1.0 / gamma))));
-                pixel.blue(clamp((int) (pixel.blue() * Math.pow(pixel.normal(), 1.0 / gamma))));
+        for (int threadId = 0; threadId < threadsCount; threadId++) {
+            final int currentThreadId = threadId;
+            int start = threadId * blockSize;
+            int end = (threadId == threadsCount - 1) ? n : (threadId + 1) * blockSize;
+
+            futures.add(executor.submit(() -> {
+                processPointsWithLocalBuffer(start, end, it, xRes, yRes, tr, threadPixels[currentThreadId]);
+                return null;
+            }));
+        }
+
+        // Ожидаем завершения потоков
+        for (Future<Void> future : futures) {
+            try {
+                future.get();
+            } catch (InterruptedException | ExecutionException e) {
+                log.error(e.getMessage());
             }
         }
+
+        executor.shutdown();
+
+        // Объединяем результаты
+        mergeThreadPixels(threadPixels, xRes, yRes);
+
+        normalizePixels(xRes, yRes);
+        return assembleImageArray(xRes, yRes);
     }
 
-    // Метод для ограничения значений цвета в пределах от 0 до 255
-    private static int clamp(int value) {
-        return Math.min(255, Math.max(0, value));
-    }
 
-    // Общая логика обработки точки и обновления пикселя
-    private static int[] processPoint(int samples, int iterations, int xRes, int yRes, Random random) {
-        int[] intPixel = new int[xRes * yRes];
-        double xRange = X_MAX - X_MIN;
-        double yRange = Y_MAX - Y_MIN;
-
-        for (int num = 0; num < samples; num++) {
-            double newX = X_MIN + xRange * random.nextDouble();
-            double newY = Y_MIN + yRange * random.nextDouble();
-
-            for (int step = -20; step < iterations; step++) {
-                int i = random.nextInt(linearTransformations.length);
-                Point point = linearTransformations[i].apply(newX, newY);
-
-                if (step >= 0 && point.x() >= X_MIN && point.x() <= X_MAX && point.y() >= Y_MIN && point.y() <= Y_MAX) {
-                    int x1 = (int) ((point.x() - X_MIN) / xRange * xRes);
-                    int y1 = (int) ((point.y() - Y_MIN) / yRange * yRes);
-
-                    if (x1 >= 0 && x1 < xRes && y1 >= 0 && y1 < yRes) {
-                        Pixel pixel = pixels[x1][y1];
-                        if (!pixel.visited()) {
-                            if (linearTransformations[i] instanceof Colorable colorable) {
-                                pixel.setColor(colorable.getRed(), colorable.getGreen(), colorable.getBlue());
-                            }
+    private static void mergeThreadPixels(Pixel[][][] threadPixels, int xRes, int yRes) {
+        for (int x = 0; x < xRes; x++) {
+            for (int y = 0; y < yRes; y++) {
+                Pixel mergedPixel = pixels[x][y];
+                for (Pixel[][] threadBuffer : threadPixels) {
+                    Pixel threadPixel = threadBuffer[x][y];
+                    if (threadPixel.counter() > 0) {
+                        if (mergedPixel.counter() == 0) {
+                            mergedPixel.setColor(threadPixel.red(), threadPixel.green(), threadPixel.blue());
+                        } else {
+                            mergedPixel.setAvg(threadPixel.red(), threadPixel.green(), threadPixel.blue());
                         }
                     }
                 }
             }
         }
+    }
 
+
+    public static int[] processPointsSingleThreaded(int n, int it, int xRes, int yRes) {
+        int tr = Checker.readPositiveInt(scanner, "Введите номер трансформации:");
+
+        for (int num = 0; num < n; num++) {
+            processPoints(num, num + 1, it, xRes, yRes, tr);
+        }
+
+        normalizePixels(xRes, yRes);
+
+        return assembleImageArray(xRes, yRes);
+    }
+
+    private static int[] assembleImageArray(int xRes, int yRes) {
+        int[] intPixel = new int[xRes * yRes];
         for (int y = 0; y < yRes; y++) {
             for (int x = 0; x < xRes; x++) {
                 Pixel pixel = pixels[x][y];
@@ -142,18 +181,107 @@ public class FractalRenderer {
         return intPixel;
     }
 
-    private static Transformation getRandomTransformation(int tr) {
+    private static void processPointsWithLocalBuffer(int start, int end, int it, int xRes, int yRes,
+        int tr, Pixel[][] localPixels) {
+        for (int num = start; num < end; num++) {
+            for (int step = -20; step < it; step++) {
+                double newX = X_MIN + (X_MAX - X_MIN) * RANDOM.nextDouble();
+                double newY = Y_MIN + (Y_MAX - Y_MIN) * RANDOM.nextDouble();
+                Point point = linearTransformations[num].apply(newX, newY);
 
-        switch (tr) {
-            case 3: return new WavesTransformation();
-            case 4: return new TangentTransformation();
-            case 5: return new CrossTransformation();
-            case 6: return new LinearTransformation();
-            case 7: return new CircularTransformation();
-            case 8: return new SwirlTransformation();
-            case 9: return new SpiralTransformation();
-            default: return new LinearTransformation();  // По умолчанию
+                point = getTransformation(tr).apply(point.x(), point.y());
+
+                if (step >= 0 && point.x() >= X_MIN && point.x() <= X_MAX && point.y() >= Y_MIN && point.y() <= Y_MAX) {
+                    int x1 = (int) (xRes - Math.floor(((X_MAX - point.x()) / (X_MAX - X_MIN)) * xRes));
+                    int y1 = (int) (yRes - Math.floor(((Y_MAX - point.y()) / (Y_MAX - Y_MIN)) * yRes));
+
+                    if (x1 >= 0 && x1 < xRes && y1 >= 0 && y1 < yRes) {
+                        Pixel pixel = localPixels[x1][y1];
+                        if (pixel.counter() == 0) {
+                            if (linearTransformations[num] instanceof Colorable colorable) {
+                                pixel.setColor(colorable.getRed(), colorable.getGreen(), colorable.getBlue());
+                            }
+                        } else {
+                            if (linearTransformations[num] instanceof Colorable colorable) {
+                                pixel.setAvg(colorable.getRed(), colorable.getGreen(), colorable.getBlue());
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 
+    private static void processPoints(int start, int end, int it, int xRes, int yRes, int tr) {
+        for (int num = start; num < end; num++) {
+            for (int step = -20; step < it; step++) {
+                double newX = X_MIN + (X_MAX - X_MIN) * RANDOM.nextDouble();
+                double newY = Y_MIN + (Y_MAX - Y_MIN) * RANDOM.nextDouble();
+                Point point = linearTransformations[num].apply(newX, newY);
+
+                point = getTransformation(tr).apply(point.x(), point.y());
+
+                if (step >= 0 && point.x() >= X_MIN && point.x() <= X_MAX && point.y() >= Y_MIN && point.y() <= Y_MAX) {
+                    int x1 = (int) (xRes - Math.floor(((X_MAX - point.x()) / (X_MAX - X_MIN)) * xRes));
+                    int y1 = (int) (yRes - Math.floor(((Y_MAX - point.y()) / (Y_MAX - Y_MIN)) * yRes));
+
+                    if (x1 >= 0 && x1 < xRes && y1 >= 0 && y1 < yRes) {
+                        synchronized (pixels[x1][y1]) {
+                            if (pixels[x1][y1].counter() == 0) {
+                                if (linearTransformations[num] instanceof Colorable colorable) {
+                                    pixels[x1][y1].setColor(colorable.getRed(), colorable.getGreen(),
+                                        colorable.getBlue());
+                                }
+                            } else {
+                                if (linearTransformations[num] instanceof Colorable colorable) {
+                                    pixels[x1][y1].setAvg(colorable.getRed(), colorable.getGreen(),
+                                        colorable.getBlue());
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private static void normalizePixels(int xRes, int yRes) {
+        double max = 0.0;
+        double gamma = 0.8;
+
+        for (int row = 0; row < xRes; row++) {
+            for (int col = 0; col < yRes; col++) {
+                if (pixels[row][col].counter() != 0) {
+                    pixels[row][col].normal(log10(pixels[row][col].counter()));
+                    if (pixels[row][col].normal() > max) {
+                        max = pixels[row][col].normal();
+                    }
+                }
+            }
+        }
+
+        for (int row = 0; row < xRes; row++) {
+            for (int col = 0; col < yRes; col++) {
+                pixels[row][col].normal(pixels[row][col].normal() / max);
+                pixels[row][col].red((int) (pixels[row][col].red() * pow(pixels[row][col].normal(), (1.0 / gamma))));
+                pixels[row][col].green(
+                    (int) (pixels[row][col].green() * pow(pixels[row][col].normal(), (1.0 / gamma))));
+                pixels[row][col].blue((int) (pixels[row][col].blue() * pow(pixels[row][col].normal(), (1.0 / gamma))));
+            }
+        }
+    }
+
+    private static Transformation getTransformation(int tr) {
+        return switch (tr) {
+            case 1 -> new HeartTransformation();
+            case 2 -> new EyeFishTransformation();
+            case 3 -> new WavesTransformation();
+            case 4 -> new TangentTransformation();
+            case 5 -> new CrossTransformation();
+            case 6 -> new CircularTransformation();
+            case 7 -> new SwirlTransformation();
+            case 8 -> new SpiralTransformation();
+            default -> new LinearTransformation();
+        };
+    }
 }
