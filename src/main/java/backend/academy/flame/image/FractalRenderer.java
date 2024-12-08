6ac2c16f8372ba0a393323ded3c0dce983ba2cf4
Howepu <125370383+Howepu@users.noreply.github.com>
2024-12-08 09:06:04 +0600

    Исправлен ввод номеров трансформаций

--- src/main/java/backend/academy/flame/image/FractalRenderer.java
+++ src/main/java/backend/academy/flame/image/FractalRenderer.java
@@ -19,7 +19,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
-import java.util.Scanner;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
